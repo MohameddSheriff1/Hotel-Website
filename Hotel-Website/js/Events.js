ba3b11ffe25fc715068063267b0caa86617b2d2e
MohameddSheriff1 <mohamed.sherifff121@gmail.com>
2025-07-08 15:13:14 +0300

    Delete Hotel-Website directory

--- Hotel-Website/js/Events.js
+++
@@ -1,7 +0,0 @@
-// Menu  of mobile
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
-
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
